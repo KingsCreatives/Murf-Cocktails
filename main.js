7d482f95caa005b4946cbc3cec7f762735ecd015
Kwame Cody <kwamecody@gmail.com>
2023-07-18 09:48:12 +0000

    updated fucntion to put drinks in the DOM

--- main.js
+++ main.js
@@ -10,19 +10,23 @@ function displayDrink(){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)
     .then(res => res.json())
     .then(data =>{
-        let drinks = data.drinks[0];
+        let drinks = data.drinks;
         console.log(drinks);
         
-        let drinkCard = document.createElement('div');
-        drinkCard.className = 'drink';
-        drinkCard.innerHTML = `
-                        <h4 class="drink-name">${drinks.strDrink}</h4>
-                        <p class="drink-info">${drinks.strInstructions}</p>
-                        <img src="${drinks.strDrinkThumb}" alt="cocktail-img" class="drink-img">`;
-    
+        drinks.forEach(drink =>{
+            console.log(drink)
+            let drinkCard = document.createElement('div');
+            drinkCard.className = 'drink';
+            drinkCard.innerHTML = `
+                            <h4 class="drink-name">${drink.strDrink}</h4>
+                            <p class="drink-info">${drink.strInstructions}</p>
+                            <img src="${drink.strDrinkThumb}" alt="cocktail-img" class="drink-img">`;
         
+            
+    
+            document.querySelector('.drinks-card').appendChild(drinkCard);
 
-        document.querySelector('.drinks-card').appendChild(drinkCard);
+        })
     })
     .catch(err =>{
         console.log(err)
