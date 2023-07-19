abc4885dc28cd51998b12aecc5fcc7ebe6fd0712
Kwame Cody <kwamecody@gmail.com>
2023-07-18 23:38:08 -0700

    displayed data with card

--- src/main.js
+++ src/main.js
@@ -14,14 +14,27 @@ function displayDrink(){
         console.log(drinks);
         
         drinks.forEach(drink =>{
-            console.log(drink)
             let drinkCard = document.createElement('div');
-            drinkCard.className = 'drink';
+            drinkCard.className = 'card drink text-center col-md-6 col-lg-6 col-sm-12';
             drinkCard.innerHTML = `
-                            <h4 class="drink-name">${drink.strDrink}</h4>
-                            <p class="drink-info">${drink.strInstructions}</p>
-                            <img src="${drink.strDrinkThumb}" alt="cocktail-img" class="drink-img">`;
+                            <img src="${drink.strDrinkThumb}" alt="cocktail-img" class="img-fluid drink-img">
+                            <div class="card-body">
+                                <h5 class="card-title">${drink.strDrink}</h5>
+                                <p class="card-text">${drink.strInstructions}</p>
+                             
+
+                            </div>
+                            `;
+
         
+                    if(drink.strVideo !== null){
+                        let link = document.createElement('a');
+                        link.href = drink.strVideo;
+                        link.className = "btn btn-secondary";
+                        link.textContent = 'Watch tutorial';
+
+                        drinkCard.insertAdjacentElement('beforeend', link)
+                    }
             
     
             document.querySelector('.drinks-card').appendChild(drinkCard);
@@ -35,3 +48,5 @@ function displayDrink(){
     // Clears form after submission
     document.querySelector('.user-form').reset()
 }
+
+
